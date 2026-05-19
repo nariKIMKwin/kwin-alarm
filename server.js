bb83214df3e6d99c98af2187a427d4c9c469a581
Na <nari9616@gmail.com>
2026-05-19 18:04:27 +0900

    add status system

--- server.js
+++ server.js
@@ -10,11 +10,36 @@ app.use(express.static("public"));
 
 let currentCalls = [];
 
+let userStatus = {
+    "김은하 실장": "in",
+    "이유빈 과장": "in",
+    "김나리 과장": "in",
+    "박규용 과장": "in"
+};
+
 io.on("connection", (socket) => {
 
     socket.emit("updateCalls", currentCalls);
+    socket.emit("updateStatus", userStatus);
+
+    socket.on("setStatus", (data) => {
+
+        userStatus[data.name] = data.status;
+
+        io.emit("updateStatus", userStatus);
+
+    });
 
     socket.on("callBell", (area) => {
+
+        if(userStatus[area] === "out"){
+
+            io.emit("absentCall", {
+                area: area
+            });
+
+            return;
+        }
 
         const exist = currentCalls.find(c => c.area === area);
 
